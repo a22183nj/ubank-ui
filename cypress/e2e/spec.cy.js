2b761b0bcb038aad6c46599977947db9b339475b
Peter Lin <a22183nj@gmail.com>
2023-02-09 13:11:01 +1100

    Minor change

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 import homePage from '../pages/homePage'
 
-describe('click search button', () => {
+describe('logo visibility', () => {
   it('passes', () => {
     cy.visit('https://www.ebay.com.au')
     homePage.logoIsVisible()
--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -1,8 +1,11 @@
 class homePage {
+    // consider using string locator instead of cy.get()
     elements = {
         logo: () => cy.get("#gh-logo")
     }
 
+    // should make isVisible() a reusable function that
+    // pass in locator as a param
     logoIsVisible() {
         this.elements.logo().should('be.visible')
     }
